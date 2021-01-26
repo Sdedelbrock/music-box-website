54feb1a8b9155b4a5a589d1751f1efec092de7f0
Shiem Edelbrock <shiem.edelbrock@me.com>
2021-01-25 20:56:48 -0700

    init

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const contentfulConfig = {
+  spaceId: 'xyy8dbrpgskd',
+  accessToken: '4FWHQxxvuLDamOnSu5H7zInbb_CsMEKX8ra_Gt04IdQ',
+}
+
+// if you want to use the preview API please define
+// CONTENTFUL_HOST in your environment config
+// the `host` property should map to `preview.contentful.com`
+// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = 'preview.contentful.com'
+}
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the access token need to be provided.'
+  )
+}
+
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Contentful starter',
+  },
+  pathPrefix: '/gatsby-contentful-starter',
+  plugins: [
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig,
+    },
+  ],
+}
